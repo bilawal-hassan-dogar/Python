346a203b7eafc098e324f01a9172db4974b65ee6
bilawal-hassan-dogar <126673862+bilawal-hassan-dogar@users.noreply.github.com>
2024-07-29 14:48:52 +0500

    Add files via upload

---
+++ cinfo.py
@@ -0,0 +1,13 @@
+from countryinfo import CountryInfo
+
+country = CountryInfo("Pakistan")
+
+data = country.alt_spellings()
+print(data)
+
+area = country.area()
+print(area)
+
+info = country.info()
+print(info)
+
